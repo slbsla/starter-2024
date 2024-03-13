a3ce55bf973d211d0a2dd15f1640eb2972006ae5
SLACHIHEB <lachihebsamir@yahoo.fr>
2024-03-14 00:35:05 +0100

    generate all tables

--- starter02-complex-hibernate/src/test/java/com/example/demo/ApplicationTests.java
+++ starter02-complex-hibernate/src/test/java/com/example/demo/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class CrudApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
