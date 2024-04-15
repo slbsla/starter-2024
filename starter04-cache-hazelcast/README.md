3d7b9efdc32717f993320668c14766af35fcb0d9
SLACHIHEB <lachihebsamir@yahoo.fr>
2024-04-15 11:40:15 +0200

    hazlcast first code not tested - WIP

---
+++ starter04-cache-hazelcast/README.md
@@ -0,0 +1,7 @@
+hz  : 4.2.8 last
+
+http://localhost:8080/swagger-ui.html
+
+ref :
+https://github.com/hazelcast-guides/caching-springboot/tree/master
+https://github.com/leszko/springboot-caching
---
+++ starter04-cache-hazelcast/src/main/java/com/slb/dev/BookService.java
@@ -0,0 +1,21 @@
+package com.slb.dev;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Cacheable("books")
+    public String getBookNameByIsbn(String isbn) {
+        return findBookInSlowSource(isbn);
+    }
+
+    private String findBookInSlowSource(String isbn) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Sample Book Name";
+    }
+}
